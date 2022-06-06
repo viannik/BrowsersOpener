feb73d589c3555373ebad9193407640839350ff1
viannik <55939563+viannik@users.noreply.github.com>
2022-06-06 18:40:33 +0200

    Update closer.py

--- closer.py
+++ closer.py
@@ -1,5 +1,5 @@
 import os
 
-os.system('taskkill /F /IM brave.exe /T > nul')
-os.system('taskkill /F /IM chrome.exe /T > nul')
-os.system('taskkill /F /IM chromedriver.exe /T > nul')
+os.system('taskkill /F /IM brave.exe /T > nul 2>&1')
+os.system('taskkill /F /IM chrome.exe /T > nul 2>&1')
+os.system('taskkill /F /IM chromedriver.exe /T > nul 2>&1')
