68c319e91d72c370a9fb8d7b248d43fad4810495
viannik <55939563+viannik@users.noreply.github.com>
2022-06-07 00:15:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os, time, subprocess
 
 
-ALL_PROFILES = 10
+ALL_PROFILES = 3
 ANTI_LAG_DELAY = 0.3
 
 prefix_for_profiles = 'test'
 chrome_exe_path = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
 
-os.system('taskkill /im chrome.exe')
+
+if input("Удалять старые вкладки [y / n]: ") == 'y':
+    os.system('taskkill /F /IM chrome.exe /T > nul 2>&1')
 
 
 def open_new_browser(number):
-    subprocess.Popen(f'"{chrome_exe_path}"' + ' --start-maximized' + f' --profile-directory={prefix_for_profiles}{number}' + ' https://uuyo.org/', shell = True)
+    subprocess.Popen(f'"{chrome_exe_path}"' + ' --start-maximized' + f' --profile-directory={prefix_for_profiles}{number}' + ' http://googleaa.com', shell = True)
     time.sleep(ANTI_LAG_DELAY)
 
 
