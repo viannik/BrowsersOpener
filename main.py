b4099264b1004d8864316bdccd33bcd371110903
viannik <55939563+viannik@users.noreply.github.com>
2022-06-06 15:34:49 +0200

    Update main.py
    Added the ability to save session changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import threading, os
+import threading, os, time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,24 +6,43 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# ANTI_LAG_DELAY = 1 # nwm
-ALL_PROFILES = 5
-URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+choice = input("Сохранять сессию или нет? [y / n]: ")
+choice = True if choice == 'y' else False
 
-#prefix_for_profiles = 'Matthew'
-#chrome_profile_path = '\\profiles_data'
+ALL_PROFILES = 3
+URL = "https://temp-mail.org"
 
-os.system('taskkill /F /IM chrome.exe /T > nul')
-os.system('taskkill /F /IM chromedriver.exe /T > nul')
+prefix_for_profiles = 'Test'
+chrome_profile_path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '\\\\') + '\\Profile Data'
 
-def open_browser_with_new_profile(number):
+os.system('taskkill /F /IM chrome.exe /T > nul 2>&1')
+os.system('taskkill /F /IM chromedriver.exe /T > nul 2>&1')
+
+
+def open_browser_with_new_profile_with_saving_session(number):
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("allow-profiles-outside-user-dir")
+    
+    options.add_experimental_option("detach", True)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    
+    options.add_argument(f"--user-data-dir={chrome_profile_path}\\Profile{number}")
+    options.add_argument(f"--profile-directory={prefix_for_profiles}{number}")
+    
+    web = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    web.get(URL)
+
+
+def open_browser_with_new_profile_without_saving_session(number):
     options = webdriver.ChromeOptions()
 
     options.add_argument('--start-maximized')
     options.add_argument("--disable-blink-features=AutomationControlled")
-
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('enable-automation')
     
     options.add_experimental_option("detach", True)
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -32,14 +51,17 @@ def open_browser_with_new_profile(number):
     
     web = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     web.get(URL)
-
+    
 
 threads = [
 
 ]
 
 for number in range(ALL_PROFILES):
-    threads.append(threading.Thread(target=open_browser_with_new_profile, args=(number, )))
+    if choice:
+        threads.append(threading.Thread(target=open_browser_with_new_profile_with_saving_session, args=(number, )))
+    else:
+        threads.append(threading.Thread(target=open_browser_with_new_profile_without_saving_session, args=(number, )))
 
 for thread in threads:
     thread.start()
