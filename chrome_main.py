bdae2f84235ba216a6c7bfdcd8adc0b5defb80ce
viannik <55939563+viannik@users.noreply.github.com>
2022-06-06 18:39:12 +0200

    Update and rename main.py to chrome_main.py
    Optimize opening

--- chrome_main.py
+++ chrome_main.py
@@ -6,14 +6,18 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-choice = input("Сохранять сессию или нет? [y / n]: ")
-choice = True if choice == 'y' else False
+choice = input("Сохранять сессию [y / n]: ")
 
-ALL_PROFILES = 3
-URL = "https://temp-mail.org"
+if choice == 'y':
+    choice = True
+else:
+    choice = False
 
-prefix_for_profiles = 'Test'
-chrome_profile_path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '\\\\') + '\\Profile Data'
+ALL_PROFILES = 25
+URL = "https://google.com"
+
+prefix_for_profiles = 'Testing'
+chrome_profile_path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '\\\\') + '\\Chrome Profile Data'
 
 os.system('taskkill /F /IM chrome.exe /T > nul 2>&1')
 os.system('taskkill /F /IM chromedriver.exe /T > nul 2>&1')
@@ -25,6 +29,11 @@ def open_browser_with_new_profile_with_saving_session(number):
     options.add_argument("--start-maximized")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("allow-profiles-outside-user-dir")
+    options.add_argument("--log-level=3")
+    options.add_argument("--mute-audio")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--disable-gpu')
     
     options.add_experimental_option("detach", True)
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -51,23 +60,13 @@ def open_browser_with_new_profile_without_saving_session(number):
     
     web = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     web.get(URL)
+
     
-
-threads = [
-
-]
-
 for number in range(ALL_PROFILES):
     if choice:
-        threads.append(threading.Thread(target=open_browser_with_new_profile_with_saving_session, args=(number, )))
+        open_browser_with_new_profile_with_saving_session(number)
     else:
-        threads.append(threading.Thread(target=open_browser_with_new_profile_without_saving_session, args=(number, )))
-
-for thread in threads:
-    thread.start()
-
-for thread in threads:
-    thread.join()
+        open_browser_with_new_profile_without_saving_session(number)
 
 os.system('cls||clear')
 print(f'Успешно открыто {ALL_PROFILES} профилей.')
