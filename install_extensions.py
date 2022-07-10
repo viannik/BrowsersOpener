323643718b853eb14e349ed5ca063c9ca323b807
viannik <55939563+viannik@users.noreply.github.com>
2022-07-10 13:05:26 +0300

    Add files via upload

--- install_extensions.py
+++ install_extensions.py
@@ -15,6 +15,11 @@ browser_registry = ''
 for i in dict_of_browsers[config['browser']['type']]:
     browser_registry += i + '\\'
 
+str_for_ping_extensions = '{ '
+for extension in extensions:
+    str_for_ping_extensions += f'"{extension}":' + ' { "toolbar_pin": "force_pinned" },'
+str_for_ping_extensions += ' }'
+
 for i in range(len(extensions)):
     registry_key = None
 
@@ -27,5 +32,8 @@ for i in range(len(extensions)):
 
     winreg.SetValueEx(registry_key, str(i + 100), 0, winreg.REG_SZ, f'{extensions[i]};https://clients2.google.com/service/update2/crx')
     winreg.CloseKey(registry_key)
+    registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, f'SOFTWARE\\Policies\\{browser_registry}', 0, winreg.KEY_WRITE)
+    winreg.SetValueEx(registry_key, 'ExtensionSettings', 0, winreg.REG_SZ, str_for_ping_extensions)
+    winreg.CloseKey(registry_key)
 
-print(f'Успешно установлено {len(extensions)} расширений!')
+print(f'Успешно установлено и закреплено {len(extensions)} расширений!')
